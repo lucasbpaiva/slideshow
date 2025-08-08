1c09196d54401feaea282b291c99d2d68006d52f
Lucas Paiva <lpaiva234@gmail.com>
2025-08-08 17:22:59 -0300

    add timeout for automatic slideshow

--- script.js
+++ script.js
@@ -23,3 +23,25 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].classList.add("active");
 }
+
+// // Automatic slideshow
+// const prev = document.querySelector(".prev");
+// const next = document.querySelector(".next");
+// prev.style.display = "none";
+// next.style.display = "none";
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let slides = document.querySelectorAll(".mySlides");
+//   let dots = document.querySelectorAll(".dot");
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//     dots[i].classList.remove("active");
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex - 1].classList.add("active");
+//   setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
